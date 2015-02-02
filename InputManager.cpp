a8d0ee25db451882d9a2ed4bf254062f33d8b33c
Pinkii- <pinkiibiza@gmail.com>
2015-02-02 16:15:17 +0100

    Added Joystick Axis support

--- InputManager.cpp
+++ InputManager.cpp
@@ -2,19 +2,25 @@
 
 std::map<std::string, InputManager::helperK> InputManager::keyboardBinds;
 std::map<std::string, InputManager::helperM> InputManager::mouseBinds;
+std::map<std::string, InputManager::helperJA> InputManager::joystickAxisBinds;
 
 void InputManager::bind(std::string s, sf::Keyboard::Key k) {
-    if (keyboardBinds.find(s) == keyboardBinds.end()) keyboardBinds.insert(std::make_pair(s,helperK(k,false)));
-    else keyboardBinds.at(s) = helperK(k,false);
+    if (keyboardBinds.find(s) == keyboardBinds.end()) keyboardBinds.insert(std::make_pair(s,helperK(k)));
+    else keyboardBinds.at(s) = helperK(k);
 }
 
 void InputManager::bind(std::string s, sf::Mouse::Button m) {
-    if (mouseBinds.find(s) == mouseBinds.end()) mouseBinds.insert(std::make_pair(s,helperM(m,false)));
-    else mouseBinds.at(s) = helperM(m,false);
+    if (mouseBinds.find(s) == mouseBinds.end()) mouseBinds.insert(std::make_pair(s,helperM(m)));
+    else mouseBinds.at(s) = helperM(m);
+}
+
+void InputManager::bind(std::string s, sf::Joystick::Axis ja, int nPad) {
+    if (joystickAxisBinds.find(s) == joystickAxisBinds.end()) joystickAxisBinds.insert(std::make_pair(s,helperJA(ja,nPad)));
+    else joystickAxisBinds.at(s) = helperJA(ja, nPad);
 }
 
 bool InputManager::isBinded(std::string s) {
-    return (keyboardBinds.find(s) != keyboardBinds.end()) || (mouseBinds.find(s) != mouseBinds.end());
+    return (keyboardBinds.find(s) != keyboardBinds.end()) || (mouseBinds.find(s) != mouseBinds.end() || joystickAxisBinds.find(s) != joystickAxisBinds.end());
 }
 
 bool InputManager::isKeyboardBinded(std::string s) {
@@ -25,11 +31,19 @@ bool InputManager::isMouseBinded(std::string s) {
     return (mouseBinds.find(s) != mouseBinds.end());
 }
 
-bool InputManager::action(std::string s) {
+bool InputManager::isJoystickBinded(std::string s) {
+    return (joystickAxisBinds.find(s) != joystickAxisBinds.end());
+}
+
+int InputManager::action(std::string s) {
     auto itK = keyboardBinds.find(s);
     auto itM = mouseBinds.find(s);
-    if (itK == keyboardBinds.end() && itM == mouseBinds.end()) return false;
-    else return (*itK).second.b || (*itM).second.b;
+    auto itJA = joystickAxisBinds.find(s);
+    if (itK == keyboardBinds.end() && itM == mouseBinds.end() && itJA == joystickAxisBinds.end()) return 0;
+    else {
+        int aux[] = {(*itK).second.b, (*itM).second.b, (*itJA).second.pos};
+        return *std::max_element(aux,aux+3);
+    }
 }
 
 void InputManager::update() {
@@ -38,5 +52,8 @@ void InputManager::update() {
     }
     for (auto& ms : mouseBinds) {
         ms.second.b = sf::Mouse::isButtonPressed(ms.second.m);
+    }
+    for (auto& jas : joystickAxisBinds) {
+        jas.second.pos = sf::Joystick::getAxisPosition(jas.second.nPad,jas.second.j);
     }
 }
--- InputManager.hpp
+++ InputManager.hpp
@@ -9,29 +9,42 @@ private:
     struct helperK {
         bool b;
         sf::Keyboard::Key k;
-        helperK(sf::Keyboard::Key kk,bool bb) {
-            b = bb;
-            k = kk;
+        helperK(sf::Keyboard::Key k) {
+            this->b = false;
+            this->k = k;
         }
     };
     struct helperM {
         bool b;
         sf::Mouse::Button m;
-        helperM(sf::Mouse::Button mm,bool bb) {
-            b = bb;
-            m = mm;
+        helperM(sf::Mouse::Button m) {
+            this->b = false;
+            this->m = m;
+        }
+    };
+    struct helperJA {
+        int pos;
+        sf::Joystick::Axis j;
+        int nPad;
+        helperJA(sf::Joystick::Axis j, int nPad) {
+            this->pos = 0;
+            this->j = j;
+            this->nPad = nPad;
         }
     };
 
     static std::map<std::string, helperK> keyboardBinds;
     static std::map<std::string, helperM> mouseBinds;
+    static std::map<std::string, helperJA> joystickAxisBinds;
 public:
     static void bind(std::string s, sf::Keyboard::Key k);
     static void bind(std::string s, sf::Mouse::Button m);
+    static void bind(std::string s, sf::Joystick::Axis ja, int nPad);
     static bool isBinded(std::string s);
     static bool isKeyboardBinded(std::string s);
     static bool isMouseBinded(std::string s);
-    static bool action(std::string s);
+    static bool isJoystickBinded(std::string s);
+    static int action(std::string s);
     static void update();
 };
 
